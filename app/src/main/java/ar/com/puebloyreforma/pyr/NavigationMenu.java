49fc7e8adff9ebea82a0c79ad962b0255ae97ce4
lucas_masciotra <lucas_masciotra@hotmail.com>
2017-12-17 23:45:38 -0300

    sdf

---
+++ app/src/main/java/ar/com/puebloyreforma/pyr/NavigationMenu.java
@@ -0,0 +1,13 @@
+package ar.com.puebloyreforma.pyr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavigationMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_menu);
+    }
+}
