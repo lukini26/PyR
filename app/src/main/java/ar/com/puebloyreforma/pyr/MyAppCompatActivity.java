2c3ebb593a3b5a62661fceb3b3c727f1cc980913
lucas_masciotra <lucas_masciotra@hotmail.com>
2017-12-21 03:21:56 -0300

    new full working menu

--- app/src/main/java/ar/com/puebloyreforma/pyr/MyAppCompatActivity.java
+++ app/src/main/java/ar/com/puebloyreforma/pyr/MyAppCompatActivity.java
@@ -1,5 +1,6 @@
 package ar.com.puebloyreforma.pyr;
 
+import android.content.Intent;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
@@ -12,6 +13,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public abstract class MyAppCompatActivity extends AppCompatActivity {
 
@@ -24,7 +26,7 @@ public abstract class MyAppCompatActivity extends AppCompatActivity {
         super.setContentView(activityParentBase);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("");
+        toolbar.setTitle("Pueblo Y Reforma");
         setSupportActionBar(toolbar);
         final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -39,14 +41,48 @@ public abstract class MyAppCompatActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
 
-                if (id == R.id.button_0) {
+                switch (id) {
+                    case R.id.calendario:
+                        Toast.makeText(MyAppCompatActivity.this,"elegiste calendario ", Toast.LENGTH_SHORT).show();
+                        Intent movef = new Intent (MyAppCompatActivity.this,CalendarActivity.class);
+                        startActivity(movef);
+                        break;
+                    case R.id.sysacad:
+                        Toast.makeText(MyAppCompatActivity.this, "elegiste tu vieja entanga", Toast.LENGTH_SHORT).show();
+                        Intent movefa = new Intent (MyAppCompatActivity.this,SysacadActivity.class);
+                        startActivity(movefa);
+                        break;
+                    case R.id.reglamento:
+                        Toast.makeText(MyAppCompatActivity.this, "elegiste ", Toast.LENGTH_SHORT).show();
+                        Intent movefew = new Intent (MyAppCompatActivity.this,PdfsActivity.class);
+                        startActivity(movefew);
+                        break;
+                    case R.id.contacto:
+                        Intent tuvieja = new Intent (MyAppCompatActivity.this,ContactoActivity.class);
+                        startActivity(tuvieja);
+                        break;
+                    case R.id.noticias:
+                        //Toast.makeText(MainActivity.this, "El tac se la come", Toast.LENGTH_SHORT).show();
+                        Intent hola = new Intent (MyAppCompatActivity.this,PermissionActivity.class);
+                        startActivity(hola);
+                        break;}
+
+
+
+
+
+
+                /*if (id == R.id.button_0) {
 
                 } else if (id == R.id.button_1) {
 
-                }
+                }*/
                 DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
                 drawer.closeDrawer(GravityCompat.START);
                 return true;
             }
         });
-    } }
+    }
+
+
+   }
