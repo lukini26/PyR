507c55a9f9b09b0536934cd501be11985dbd0afe
lucas_masciotra <lucas_masciotra@hotmail.com>
2018-03-26 18:17:36 -0300

    prod mas planes para que quede

---
+++ app/src/main/java/ar/com/puebloyreforma/pyr/CustomscheActivity.java
@@ -0,0 +1,13 @@
+package ar.com.puebloyreforma.pyr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomscheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customsche);
+    }
+}
