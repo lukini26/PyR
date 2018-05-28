47db1d6db27092b980537b0ac5a871b76eeb06f6
lukini <lucas_masciotra@hotmail.com>
2018-05-28 10:38:31 -0300

    calendar spinner

--- app/src/main/java/ar/com/puebloyreforma/pyr/CalendarActivity.java
+++ app/src/main/java/ar/com/puebloyreforma/pyr/CalendarActivity.java
@@ -6,9 +6,13 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
-public class CalendarActivity extends MyAppCompatActivity {
+public class CalendarActivity extends MyAppCompatActivity implements AdapterView.OnItemSelectedListener {
     ViewPager viewPager ;
     ViewPagerAdapter adapter ;
         private  Integer[] images = {R.drawable.referencias01 ,
@@ -31,6 +35,32 @@ public class CalendarActivity extends MyAppCompatActivity {
         viewPager.setAdapter(adapter) ;
         Calendar calendar = Calendar.getInstance() ;
         int thisMonth = calendar.get(Calendar.MONTH);
-        viewPager.setCurrentItem(thisMonth-1);
+
+        Spinner spinner = (Spinner) findViewById(R.id.meses_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.meses_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(thisMonth-1);
+        spinner.setOnItemSelectedListener(this);
+
+
+
     }
+
+    public void onItemSelected(final AdapterView<?>  parent, View view,
+                                int pos, long id) {
+
+                    viewPager.setCurrentItem(pos);
+
+                }
+
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
 }
